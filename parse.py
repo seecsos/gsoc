8f52058bdc97a04ffd2aea22f1324cbdac373428
Usman <usmanwardag@gmail.com>
2016-10-12 17:45:31 +0500

    Scraped 2015 data

--- parse.py
+++ parse.py
@@ -1,8 +1,8 @@
-import time
-import urllib2
+import csv
 from bs4 import BeautifulSoup
+import urllib2
 
-def get_site_html(url):
+def _get_site_html(url):
     """Read an html page with the right settings."""
 
     hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Ge\cko)' 
@@ -21,10 +21,10 @@ def get_site_html(url):
 
     return source
 
-def get_tree(url):
+def _get_tree(url):
     """Parse an HTML page into a BeautifulSoup tree."""
 
-    source = get_site_html(url)
+    source = _get_site_html(url)
     try:
       tree = BeautifulSoup(source,'html.parser')
     except:
@@ -32,15 +32,16 @@ def get_tree(url):
     
     return tree
     
-def get_results(tree):
-    
+def _get_results(tree, year):
+    """Parse results as tuples."""
     projects = []
     
-    for t in tree.findAll('li', {'class': 'mdl-list__item mdl-list__item--one-line'}):
-        org = t.text.replace('\n', '')
+    for t in tree.findAll('li', {'class': 'mdl-list__item mdl-list__item--one-line'}):   
+        org = _clean(t.text)
+        print org
         a = t.findChildren('a')[0]['href']
         org_url = 'https://www.google-melange.com' + a
-        org_tree = get_tree(org_url)
+        org_tree = _get_tree(org_url)
 
         for t1 in org_tree.findAll('span', {'class': 'mdl-list__item-primary-content'}):
             a1 = t1.findChildren('a')
@@ -48,26 +49,37 @@ def get_results(tree):
 
             for p in projs:
                 proj_url = 'https://www.google-melange.com' + p
-                proj_tree = get_tree(proj_url)
-                title = proj_tree.findAll('h3')[0]
+                proj_tree = _get_tree(proj_url)
+                title = _clean(proj_tree.findAll('h3')[0].text)
                 p = proj_tree.findAll('p')
-                bio = p[0].text.replace('\n', '')
-                description = p[1].text.replace('\n', '')
-                student = bio.split('by')[-1].split('for')[0].replace("  ", "")
-                description = description.replace("  ", "")
+                bio = _clean(p[0].text)
+                student = bio.split('by')[-1].split('for')[0]
+                description = _clean(p[1].text)
+                projects.append((title, org, student, description))
 
-                projects.append((title, description, student, org))
+    _save_results(projects, year)
 
-    return projects
+def _clean(str):
+    return str.replace('\n', '').replace("  ", "").encode("utf-8")
 
-def save_results(projects):
 
-    pass
-            
-def main():
-    url = 'https://www.google-melange.com/archive/gsoc/2015'
-    tree = get_tree(url)
-    get_results(tree)
+def _save_results(projects, year):
+
+    titles = [p[0] for p in projects]
+    descriptions = [p[1] for p in projects]
+    students = [p[2] for p in projects]
+    orgs = [p[3] for p in projects]
+    rows = zip(titles, descriptions, students, orgs)
+
+    with open('data/data'+year+'.csv', 'wb') as f:
+        writer = csv.writer(f)
+        for row in rows:
+            writer.writerow(row)
+
+def parse(year):
+    url = 'https://www.google-melange.com/archive/gsoc/' + year
+    tree = _get_tree(url)
+    _get_results(tree, year)
 
 if __name__ == '__main__':
-    main()
+    parse('2015')
